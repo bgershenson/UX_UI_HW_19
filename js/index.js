1470e8803cf12e3edc8232af4fe78107bad001d0
bgershenson <74573538+bgershenson@users.noreply.github.com>
2021-04-30 18:05:12 -0700

    jquery attempt 1

---
+++ js/index.js
@@ -0,0 +1,23 @@
+console.log("It must be working player");
+
+$(".projectOne").hover(function() {
+    $(this).css("opacity", "0.79");
+}, function() {
+    $(this).css("opacity", "1");
+}
+
+);
+
+
+
+
+$(".projectTwo").hover(function() {
+    $(this).css("background-color", white);
+
+})
+
+
+$(".projectThree").hover(function() {
+    $(this).css("background-color", white);
+
+})
